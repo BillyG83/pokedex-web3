4303461517d1c9834c23069b78e71c709a4871c8
billgil <bill@evergonlabs.com>
2024-05-20 14:10:57 +0100

    chore: Pokemon single page, added error protection

--- app/[pokemon]/page.tsx
+++ app/[pokemon]/page.tsx
@@ -5,11 +5,15 @@ import { PokemonSubset } from '@/types'
 import { CollectButton } from '../components'
 
 async function getData(pokemon: string) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    return res.json()
+  } catch (error) {
+    console.error(error)
   }
-  return res.json()
 }
 
 const Pokemon = async ({ params }: { params: { pokemon: string } }) => {
@@ -18,28 +22,31 @@ const Pokemon = async ({ params }: { params: { pokemon: string } }) => {
   return (
     <section className={styles.main}>
       <h1>{params.pokemon}</h1>
-      <p>Base Exp: {res.base_experience}</p>
-      <p>Height: {res.height}cm</p>
-      <p>Weight: {res.weight}kg</p>
-      <p>Pokedex Number: {res.id}</p>
+      {res.base_experience && <p>Base Exp: {res.base_experience}</p>}
+      {res.height && <p>Height: {res.height}cm</p>}
+      {res.weight && <p>Weight: {res.weight}kg</p>}
+      {res.id && <p>Pokedex Number: {res.id}</p>}
       <p>
         Type:{' '}
-        {res.types.map((stat) => (
+        {res.types?.map((stat) => (
           <span className={style.stat} key={stat.type.url}>
             {stat.type.name}
           </span>
         ))}
       </p>
-      <Image
-        alt={params.pokemon}
-        height={150}
-        src={res.sprites.front_default}
-        width={150}
-      />
+      {res.sprites.front_default && (
+        <Image
+          alt={params.pokemon}
+          height={150}
+          src={res.sprites.front_default}
+          width={150}
+        />
+      )}
+
       <div>
         Abilities:
         <ul>
-          {res.abilities.map((item) => (
+          {res.abilities?.map((item) => (
             <li key={item.ability.url}>{item.ability.name}</li>
           ))}
         </ul>
