51827eee580fb727911f3feb9f24d4062eb0e1d3
billgil <bill@evergonlabs.com>
2024-05-20 17:36:08 +0100

    util: added const enums

---
+++ const/links.ts
@@ -0,0 +1,9 @@
+export enum EUrl {
+  IMAGES = 'https://www.google.com/search?q=',
+  VIDEO = 'https://www.youtube.com/@pokemon/search?query=',
+  WIKI = 'https://bulbapedia.bulbagarden.net/wiki/',
+}
+
+export enum ERoute {
+  HOME = '/',
+}
