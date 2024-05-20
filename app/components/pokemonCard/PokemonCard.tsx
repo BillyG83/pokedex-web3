a637b234c57de668a64408dc58d1abb7592d4401
billgil <bill@evergonlabs.com>
2024-05-20 17:37:38 +0100

    chore(PokemonCard): clean up, more reusable

--- app/components/pokemonCard/PokemonCard.tsx
+++ app/components/pokemonCard/PokemonCard.tsx
@@ -2,51 +2,36 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 import { TPokemonSubset } from '@/types'
-import {
-  Card,
-  buttonBaseStyles,
-  buttonPrimaryStyles,
-  buttonSecondaryStyles,
-} from '@/ui'
+import { Card, buttonBaseStyles, buttonSecondaryStyles } from '@/ui'
+import { CollectButton } from '../collectButton'
 
 const PokemonCard = (props: TPokemonSubset) => {
   const extraInfo = `A ${props.types[0].type.name} type pokemon with a base exp of ${props.base_experience} and moves including ${props.abilities[0].ability.name}!`
   return (
     <Card
       title={props.name}
+      description={extraInfo}
       cta={
         <>
-          <Link
-            className={clsx(
-              buttonBaseStyles.buttonBase,
-              buttonPrimaryStyles.buttonPrimary
-            )}
-            href={props.name}
-          >
-            Collect
-          </Link>
+          <CollectButton pokemonId={String(props.id)} />
           <Link
             className={clsx(
               buttonBaseStyles.buttonBase,
               buttonSecondaryStyles.buttonSecondary
             )}
-            href={`https://bulbapedia.bulbagarden.net/wiki/${props.name}`}
-            target="_blank"
+            href={props.name}
           >
             Details &#8594;
           </Link>
         </>
       }
-      description={extraInfo}
     >
-      {props.sprites.front_default && (
-        <Image
-          alt={props.name}
-          height={150}
-          src={props.sprites.front_default}
-          width={150}
-        />
-      )}
+      <Image
+        alt={props.name}
+        height={250}
+        src={props.sprites.front_default}
+        width={250}
+      />
     </Card>
   )
 }
